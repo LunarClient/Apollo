fadd4a6ad0d5d6792539de4c00c8ef6773749d58
GeckoThePecko <bernie.gandin2@gmail.com>
2022-08-04 14:19:34 -0400

    apollo: Setup local maven publishing with sources

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -65,3 +65,17 @@ dependencies {
     // shaded in dependencies
     api("com.google.protobuf:protobuf-java:3.21.4")
 }
+
+val sourcesJar by tasks.registering(Jar::class) {
+    classifier = "sources"
+    from(sourceSets.main.get().allSource)
+}
+
+publishing {
+    publications {
+        register("mavenJava", MavenPublication::class) {
+            from(components["java"])
+            artifact(sourcesJar)
+        }
+    }
+}
