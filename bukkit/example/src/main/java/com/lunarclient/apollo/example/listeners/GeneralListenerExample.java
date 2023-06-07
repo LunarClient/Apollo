b4ae54bbc321b6ed56e0a191ea06c03ceccad9c8
ItsNature <matej.bucaric@gmail.com>
2023-06-07 16:15:07 +0200

    docs: add events docs

--- bukkit/example/src/main/java/com/lunarclient/apollo/example/listeners/GeneralListenerExample.java
+++ bukkit/example/src/main/java/com/lunarclient/apollo/example/listeners/GeneralListenerExample.java
@@ -1,9 +1,12 @@
 package com.lunarclient.apollo.example.listeners;
 
+import com.lunarclient.apollo.event.Event;
 import com.lunarclient.apollo.event.EventBus;
+import com.lunarclient.apollo.event.EventCancellable;
 import com.lunarclient.apollo.event.Listen;
 import com.lunarclient.apollo.event.ApolloListener;
 import com.lunarclient.apollo.event.player.ApolloRegisterPlayerEvent;
+import com.lunarclient.apollo.player.ApolloPlayer;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.entity.Player;
@@ -19,7 +22,7 @@ public class GeneralListenerExample {
 
         @Listen
         public void onApolloRegister(ApolloRegisterPlayerEvent event) {
-            ((Player) event.getPlayer()).sendMessage(Component.text("You have joined using LunarClient!", NamedTextColor.GREEN));
+            ((Player) event.getPlayer()).sendMessage("You have joined using LunarClient!");
         }
     }
 
@@ -31,7 +34,75 @@ public class GeneralListenerExample {
         }
 
         public void onApolloRegister(ApolloRegisterPlayerEvent event) {
-            ((Player) event.getPlayer()).sendMessage(Component.text("You have joined using LunarClient!", NamedTextColor.GREEN));
+            ((Player) event.getPlayer()).sendMessage("You have joined using LunarClient!");
         }
+    }
+
+    // Normal event
+    public class CoolApolloEvent implements Event {
+
+        private final ApolloPlayer player;
+
+        public CoolApolloEvent(ApolloPlayer player) {
+            this.player = player;
+        }
+
+        public ApolloPlayer getPlayer() {
+            return this.player;
+        }
+    }
+
+    // Cancellable event
+    public class CoolApolloCancellableEvent implements EventCancellable {
+
+        private final ApolloPlayer player;
+
+        private boolean cancelled;
+
+        public CoolApolloCancellableEvent(ApolloPlayer player) {
+            this.player = player;
+        }
+
+        public ApolloPlayer getPlayer() {
+            return this.player;
+        }
+
+        @Override
+        public boolean isCancelled() {
+            return this.cancelled;
+        }
+
+        @Override
+        public void setCancelled(boolean cancel) {
+            this.cancelled = cancel;
+        }
+    }
+
+    public class ApolloExampleHandler {
+
+        // Calling a normal event
+        public void callCoolApolloEvent(ApolloPlayer player) {
+            CoolApolloEvent event = new CoolApolloEvent(player);
+            EventBus.EventResult<CoolApolloEvent> result = EventBus.getBus().post(event);
+
+            for (Throwable throwable : result.getThrowing()) {
+                throwable.printStackTrace();
+            }
+        }
+
+        // Calling a cancellable event
+        public void callCoolApolloCancellableEvent(ApolloPlayer player) {
+            CoolApolloCancellableEvent event = new CoolApolloCancellableEvent(player);
+            EventBus.EventResult<CoolApolloCancellableEvent> result = EventBus.getBus().post(event);
+
+            if (!result.getEvent().isCancelled()) {
+                // Do some action if the event is not cancelled
+            }
+
+            for (Throwable throwable : result.getThrowing()) {
+                throwable.printStackTrace();
+            }
+        }
+
     }
 }
