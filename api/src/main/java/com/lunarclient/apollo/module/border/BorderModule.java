53f26dd5f4dd981d32904bfcfef18aa54c49a39b
ItsNature <matej.bucaric@gmail.com>
2023-07-12 17:43:49 +0200

    docs: fix typo

--- api/src/main/java/com/lunarclient/apollo/module/border/BorderModule.java
+++ api/src/main/java/com/lunarclient/apollo/module/border/BorderModule.java
@@ -52,7 +52,7 @@ public abstract class BorderModule extends ApolloModule {
     /**
      * Removes the {@link Border} from the {@link Audience}.
      *
-     * @param audience the player who is receiving the packet
+     * @param audience the audience that is receiving the packet
      * @param borderId the border id
      * @since 1.0.0
      */
