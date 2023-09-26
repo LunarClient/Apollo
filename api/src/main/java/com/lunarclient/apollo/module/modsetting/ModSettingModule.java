17f0cab71561767dc87e50fbfb0b30d36648a2b7
Matej Bučarić <29950356+ItsNature@users.noreply.github.com>
2023-09-26 23:01:21 +0200

    Feature - Mod Settings (#35)
    * Revert "api: remove mod settings"
    
    This reverts commit d306fa38b3a3ff7f2b8c761ec6370d9b54a171c7.
    
    * fix imports
    
    * Start working on new mod settings implementation
    
    * docs: add new examples
    
    * register mod options
    
    * add rollback mod setting example

---
+++ api/src/main/java/com/lunarclient/apollo/module/modsetting/ModSettingModule.java
@@ -0,0 +1,73 @@
+/*
+ * This file is part of Apollo, licensed under the MIT License.
+ *
+ * Copyright (c) 2023 Moonsworth
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.lunarclient.apollo.module.modsetting;
+
+import com.lunarclient.apollo.ApolloPlatform;
+import com.lunarclient.apollo.mods.Mods;
+import com.lunarclient.apollo.module.ApolloModule;
+import com.lunarclient.apollo.module.ModuleDefinition;
+import com.lunarclient.apollo.option.Option;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ * Represents the mod settings module.
+ *
+ * @since 1.0.0
+ */
+@ModuleDefinition(id = "mod_setting", name = "Mod Setting")
+public final class ModSettingModule extends ApolloModule {
+
+    ModSettingModule() {
+        this.registerModOptions();
+    }
+
+    @Override
+    public Collection<ApolloPlatform.Kind> getSupportedPlatforms() {
+        return Arrays.asList(ApolloPlatform.Kind.SERVER, ApolloPlatform.Kind.PROXY);
+    }
+
+    @Override
+    public boolean isClientNotify() {
+        return true;
+    }
+
+    private void registerModOptions() {
+        for (Class<?> mod : Mods.ALL_MODS) {
+            Field[] fields = mod.getDeclaredFields();
+
+            for (Field field : fields) {
+                try {
+                    field.setAccessible(true);
+                    Option<?, ?, ?> option = (Option<?, ?, ?>) field.get(Option.class);
+                    this.registerOptions(option);
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+}
