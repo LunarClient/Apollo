74df641a5874e998f028c265c6250a0cdff2589d
ItsNature <matej.bucaric@gmail.com>
2023-12-11 16:46:32 +0100

    Add EVNT command

---
+++ bukkit-example/src/main/java/com/lunarclient/apollo/example/commands/EVNTCommand.java
@@ -0,0 +1,131 @@
+/*
+ * This file is part of Apollo, licensed under the MIT License.
+ *
+ * Copyright (c) 2023 Moonsworth
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.lunarclient.apollo.example.commands;
+
+import com.lunarclient.apollo.example.ApolloExamplePlugin;
+import com.lunarclient.apollo.example.modules.EVNTExample;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class EVNTCommand implements CommandExecutor {
+
+    private final EVNTExample evntExample = ApolloExamplePlugin.getPlugin().getEvntExample();
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("Player only!");
+            return true;
+        }
+
+        Player player = (Player) sender;
+
+        if (args.length != 1) {
+            player.sendMessage("Usage: /evnt <overrideHeartTexture" +
+                "|resetHeartTexture" +
+                "|openGui" +
+                "|closeGui" +
+                "|overrideCharacter" +
+                "|overrideCharacterAbility" +
+                "|overrideCharacterCosmetic" +
+                "|updateGameOverview" +
+                "|updateStatusOverview>");
+            return true;
+        }
+
+        switch (args[0].toLowerCase()) {
+            case "overridehearttexture": {
+                this.evntExample.overrideHeartTextureExample(player);
+                player.sendMessage("Displaying heart texture...");
+                break;
+            }
+
+            case "resethearttexture": {
+                this.evntExample.resetHeartTextureExample(player);
+                player.sendMessage("Resetting heart texture...");
+                break;
+            }
+
+            case "opengui": {
+                this.evntExample.openGuiExample(player);
+                player.sendMessage("Opening GUI...");
+                break;
+            }
+
+            case "closegui": {
+                this.evntExample.closeGuiExample(player);
+                player.sendMessage("Closing GUI...");
+                break;
+            }
+
+            case "overridecharacter": {
+                this.evntExample.overrideCharacterExample(player);
+                player.sendMessage("Overriding character...");
+                break;
+            }
+
+            case "overridecharacterability": {
+                this.evntExample.overrideCharacterAbilityExample(player);
+                player.sendMessage("Overriding character ability...");
+                break;
+            }
+
+            case "overridecharactercosmetic": {
+                this.evntExample.overrideCharacterCosmeticExample(player);
+                player.sendMessage("Overriding character cosmetic...");
+                break;
+            }
+
+            case "updategameoverview": {
+                this.evntExample.updateGameOverviewExample();
+                player.sendMessage("Updaing game overview...");
+                break;
+            }
+
+            case "updatestatusoverview": {
+                this.evntExample.updateStatusOverviewExample();
+                player.sendMessage("Updaing status overview...");
+                break;
+            }
+
+            default: {
+                player.sendMessage("Usage: /evnt <overrideHeartTexture" +
+                    "|resetHeartTexture" +
+                    "|openGui" +
+                    "|closeGui" +
+                    "|overrideCharacter" +
+                    "|overrideCharacterAbility" +
+                    "|overrideCharacterCosmetic" +
+                    "|updateGameOverview" +
+                    "|updateStatusOverview>");
+                break;
+            }
+        }
+
+        return true;
+    }
+}
--- bukkit-example/src/main/java/com/lunarclient/apollo/example/modules/EVNTExample.java
+++ bukkit-example/src/main/java/com/lunarclient/apollo/example/modules/EVNTExample.java
@@ -25,28 +25,101 @@ package com.lunarclient.apollo.example.modules;
 
 import com.google.common.collect.Sets;
 import com.lunarclient.apollo.Apollo;
+import com.lunarclient.apollo.module.evnt.Character;
+import com.lunarclient.apollo.module.evnt.CharacterAbility;
 import com.lunarclient.apollo.module.evnt.CharacterType;
 import com.lunarclient.apollo.module.evnt.EVNTModule;
+import com.lunarclient.apollo.module.evnt.GuiType;
 import com.lunarclient.apollo.module.evnt.event.EventGame;
 import com.lunarclient.apollo.module.evnt.event.EventPlayer;
 import com.lunarclient.apollo.module.evnt.event.EventStatus;
 import com.lunarclient.apollo.module.evnt.event.EventTeam;
 import com.lunarclient.apollo.player.ApolloPlayer;
 import com.lunarclient.apollo.recipients.Recipients;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.entity.Player;
+
+import java.awt.*;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class EVNTExample {
 
     private final EVNTModule evntModule = Apollo.getModuleManager().getModule(EVNTModule.class);
 
+    public void overrideHeartTextureExample(Player viewer) {
+        Optional<ApolloPlayer> apolloPlayerOpt = Apollo.getPlayerManager().getPlayer(viewer.getUniqueId());
+
+        apolloPlayerOpt.ifPresent(apolloPlayer -> {
+            ThreadLocalRandom random = ThreadLocalRandom.current();
+
+            this.evntModule.overrideHeartTexture(apolloPlayer,
+                random.nextInt(5, 15),
+                random.nextBoolean());
+        });
+    }
+
+    public void resetHeartTextureExample(Player viewer) {
+        Optional<ApolloPlayer> apolloPlayerOpt = Apollo.getPlayerManager().getPlayer(viewer.getUniqueId());
+        apolloPlayerOpt.ifPresent(this.evntModule::resetHeartTexture);
+    }
+
+    public void openGuiExample(Player viewer) {
+        Optional<ApolloPlayer> apolloPlayerOpt = Apollo.getPlayerManager().getPlayer(viewer.getUniqueId());
+
+        apolloPlayerOpt.ifPresent(apolloPlayer -> {
+            GuiType[] values = GuiType.values();
+            int randomGuiType = ThreadLocalRandom.current().nextInt(values.length);
+
+            this.evntModule.openGui(apolloPlayer, values[randomGuiType]);
+        });
+    }
+
+    public void closeGuiExample(Player viewer) {
+        Optional<ApolloPlayer> apolloPlayerOpt = Apollo.getPlayerManager().getPlayer(viewer.getUniqueId());
+        apolloPlayerOpt.ifPresent(this.evntModule::closeGui);
+    }
+
+    public void overrideCharacterExample(Player viewer) {
+        Optional<ApolloPlayer> apolloPlayerOpt = Apollo.getPlayerManager().getPlayer(viewer.getUniqueId());
+
+        apolloPlayerOpt.ifPresent(apolloPlayer -> {
+            this.evntModule.overrideCharacter(apolloPlayer, Character.builder()
+                    .playerUuid(apolloPlayer.getUniqueId())
+                    .type(CharacterType.ANGEL)
+                    .color(Color.RED)
+                    .equipped(true)
+                .build());
+        });
+    }
+
+    public void overrideCharacterAbilityExample(Player viewer) {
+        Optional<ApolloPlayer> apolloPlayerOpt = Apollo.getPlayerManager().getPlayer(viewer.getUniqueId());
+
+        apolloPlayerOpt.ifPresent(apolloPlayer -> {
+            List<CharacterAbility> abilities = Collections.singletonList(
+                CharacterAbility.builder()
+                    .type(CharacterType.ANGEL)
+                    .abilities(Collections.singletonList(
+                        Component.text("Angel", NamedTextColor.BLUE)
+                    ))
+                    .build()
+            );
+
+            this.evntModule.overrideCharacterAbility(apolloPlayer, abilities);
+        });
+    }
+
     public void overrideCharacterCosmeticExample(Player viewer) {
         Optional<ApolloPlayer> apolloPlayerOpt = Apollo.getPlayerManager().getPlayer(viewer.getUniqueId());
 
         apolloPlayerOpt.ifPresent(apolloPlayer -> {
-            this.evntModule.overrideCharacterCosmetic(Recipients.ofEveryone(), apolloPlayer.getUniqueId(), CharacterType.ANGEL);
+            this.evntModule.overrideCharacterCosmetic(apolloPlayer, apolloPlayer.getUniqueId(), CharacterType.ANGEL);
         });
     }
 
