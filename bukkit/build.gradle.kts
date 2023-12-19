c48c0c311b136366b1733e6919a9aa5bc989e95b
connorhartley <vectrixu@gmail.com>
2023-12-20 00:39:08 +1300

    publish shaded commons

--- bukkit/build.gradle.kts
+++ bukkit/build.gradle.kts
@@ -11,7 +11,7 @@ dependencies {
     compileOnly(libs.bukkit)
 
     api(project(path = ":apollo-api", configuration = "shadow"))
-    api(project(":apollo-common"))
+    api(project(path = ":apollo-common", configuration = "shadow"))
 
     "loaderCompileOnly"(libs.bukkit.api)
     "loaderImplementation"(project(":extra:apollo-extra-loader"))
--- velocity/build.gradle.kts
+++ velocity/build.gradle.kts
@@ -6,7 +6,7 @@ dependencies {
     compileOnly(libs.velocity)
 
     api(project(path = ":apollo-api", configuration = "shadow"))
-    api(project(":apollo-common"))
+    api(project(path = ":apollo-common", configuration = "shadow"))
     api(project(":extra:apollo-extra-adventure4"))
 
     annotationProcessor(libs.velocity)
