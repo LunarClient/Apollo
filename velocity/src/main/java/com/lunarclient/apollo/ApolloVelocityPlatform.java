8a932505f4faa51ef669bded8fe7eadb6a8079f4
ItsNature <matej.bucaric@gmail.com>
2024-01-01 18:39:57 +0100

    Bump to 1.0.6

--- velocity/src/main/java/com/lunarclient/apollo/ApolloVelocityPlatform.java
+++ velocity/src/main/java/com/lunarclient/apollo/ApolloVelocityPlatform.java
@@ -92,7 +92,7 @@ import lombok.Getter;
 @Plugin(
     id = "apollo",
     name = "Apollo-Velocity",
-    version = "1.0.5",
+    version = "1.0.6",
     url = "https://moonsworth.com",
     description = "Implementation of Apollo for Velocity",
     authors = {"Moonsworth"}
