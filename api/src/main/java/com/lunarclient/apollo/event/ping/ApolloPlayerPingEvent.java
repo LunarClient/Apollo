e0a317b7317b5d0a3292374c1f0ad31ce9776e45
AppleDash <pet@feather.horse>
2024-02-29 21:00:00 -0500

    docs(player-ping): add some documentation and code formatting

--- api/src/main/java/com/lunarclient/apollo/event/ping/ApolloPlayerPingEvent.java
+++ api/src/main/java/com/lunarclient/apollo/event/ping/ApolloPlayerPingEvent.java
@@ -6,9 +6,27 @@ import lombok.Value;
 
 import java.util.UUID;
 
+/**
+ * An event that is fired when a player ping message is received from a player.
+ *
+ * @since 1.1.1
+ */
 @Value
 public class ApolloPlayerPingEvent implements Event {
+
+    /**
+     * @return The UUID of the player who sent the ping.
+     */
     UUID playerUuid;
+
+    /**
+     * @return The in-world location of the ping.
+     */
     ApolloLocation location;
+
+    /**
+     * @return true if the ping is a double-click ping
+     */
     boolean isDoublePing;
+
 }
--- api/src/main/java/com/lunarclient/apollo/module/ping/PingModule.java
+++ api/src/main/java/com/lunarclient/apollo/module/ping/PingModule.java
@@ -8,8 +8,24 @@ import org.jetbrains.annotations.ApiStatus;
 
 import java.util.UUID;
 
+/**
+ * Represents the Player Ping module.
+ *
+ * @since 1.1.1
+ */
 @ApiStatus.NonExtendable
 @ModuleDefinition(id = "ping", name = "Ping")
 public abstract class PingModule extends ApolloModule {
+
+    /**
+     * Send a team ping to all the given Recipients.
+     *
+     * @param recipients the recipients to send the packet to
+     * @param playerUuid the player UUID who has originally sent the ping
+     * @param location  the in-world location of the ping
+     * @param isDoublePing true if the ping is a double-click ping, false otherwise
+     * @since 1.1.1
+     */
     public abstract void pingTeamMembers(Recipients recipients, UUID playerUuid, ApolloLocation location, boolean isDoublePing);
+
 }
