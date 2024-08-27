6c63dc62b46ce965bf77f34d8cc8401241980515
Matej Bučarić <matej.bucaric@gmail.com>
2024-08-27 10:39:00 +0200

    Lightweight (#162)
    * WIP: lightweight docs
    
    * Lightweight doc revamp
    
    * Simple touch-ups
    
    * Switch methods to tabs
    
    * Attempt to fix tabs
    
    * Close tabs
    
    * again
    
    * Revert tabs
    
    * Attempt to add tabs once more
    
    * Add tab import
    
    * Fix tab import
    
    * Add lightweight-wrapper & lightweight-example modules
    
    * Register "lunar:apollo" for player detection
    
    * Add protobuf repo section
    
    * Remove lightweight-wrapper & lightweight-example
    
    * Add the Apollo repository & protobuf-java-util as dependency to docs
    
    * Use correct `
    
    * Fix typo
    
    ---------
    
    Co-authored-by: TrentinTheKid <25537885+TrentinTheKid@users.noreply.github.com>

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 # Deploy apollo to the test server
 set -e
 ./gradlew clean build
-scp bukkit/build/libs/apollo-bukkit-1.1.5-SNAPSHOT.jar ubuntu@build.moonsworth.com:/home/ubuntu/apollo/plugins/
-scp bukkit-example/build/libs/apollo-bukkit-example-1.1.5-SNAPSHOT.jar ubuntu@build.moonsworth.com:/home/ubuntu/apollo/plugins/
+scp bukkit/build/libs/apollo-bukkit-1.1.5-SNAPSHOT.jar ubuntu@147.135.8.94:/home/ubuntu/apollo/plugins/
+scp bukkit-example/build/libs/apollo-bukkit-example-1.1.5-SNAPSHOT.jar ubuntu@147.135.8.94:/home/ubuntu/apollo/plugins/
+scp bukkit/build/libs/apollo-bukkit-1.1.5-SNAPSHOT.jar ubuntu@147.135.8.94:/home/ubuntu/lctest/plugins/
+scp bukkit-example/build/libs/apollo-bukkit-example-1.1.5-SNAPSHOT.jar ubuntu@147.135.8.94:/home/ubuntu/lctest/plugins/
