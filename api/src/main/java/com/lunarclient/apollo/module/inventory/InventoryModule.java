5bfef746e57d82d51f801ce50f7f4f17cfd4f1e7
Matej Bučarić <matej.bucaric@gmail.com>
2025-03-25 19:09:44 +0100

    Feature - NBT Apollo Modules (#195)
    * Add Glint, Saturation & Inventory Apollo Modules
    
    * Disable Inventory Module by default

---
+++ api/src/main/java/com/lunarclient/apollo/module/inventory/InventoryModule.java
@@ -0,0 +1,48 @@
+/*
+ * This file is part of Apollo, licensed under the MIT License.
+ *
+ * Copyright (c) 2023 Moonsworth
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.lunarclient.apollo.module.inventory;
+
+import com.lunarclient.apollo.module.ApolloModule;
+import com.lunarclient.apollo.module.ModuleDefinition;
+import org.jetbrains.annotations.ApiStatus;
+
+/**
+ * Represents the inventory module.
+ *
+ * @since 1.1.7
+ */
+@ApiStatus.NonExtendable
+@ModuleDefinition(id = "inventory", name = "Inventory")
+public class InventoryModule extends ApolloModule {
+
+    protected InventoryModule() {
+        this.registerOptions(ApolloModule.ENABLE_OPTION_OFF);
+    }
+
+    @Override
+    public boolean isClientNotify() {
+        return true;
+    }
+
+}
