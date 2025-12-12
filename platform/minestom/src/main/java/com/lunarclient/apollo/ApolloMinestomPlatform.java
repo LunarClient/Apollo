97cb301a099e42b91a601f1f9fe0bda4868f13f3
Matej Bučarić <matej.bucaric@gmail.com>
2025-12-12 20:17:30 +0100

    Bump to 1.2.1 (#245)

--- platform/minestom/src/main/java/com/lunarclient/apollo/ApolloMinestomPlatform.java
+++ platform/minestom/src/main/java/com/lunarclient/apollo/ApolloMinestomPlatform.java
@@ -237,7 +237,7 @@ public final class ApolloMinestomPlatform implements ApolloPlatform {
 
     @Override
     public String getApolloVersion() {
-        return "1.2.0";
+        return "1.2.1";
     }
 
     @Override
--- platform/velocity/src/main/java/com/lunarclient/apollo/ApolloVelocityPlatform.java
+++ platform/velocity/src/main/java/com/lunarclient/apollo/ApolloVelocityPlatform.java
@@ -105,7 +105,7 @@ import lombok.Getter;
 @Plugin(
     id = "apollo",
     name = "Apollo-Velocity",
-    version = "1.2.0",
+    version = "1.2.1",
     url = "https://moonsworth.com",
     description = "Implementation of Apollo for Velocity",
     authors = {"Moonsworth"}
