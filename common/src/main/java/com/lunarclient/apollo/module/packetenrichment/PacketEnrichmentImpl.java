fa0baaab742986659a5cbd07c2d9e88c79abc5b7
ItsNature <matej.bucaric@gmail.com>
2026-02-05 03:17:50 +0100

    Add additional Packet Enrichment config options for each packet

--- common/src/main/java/com/lunarclient/apollo/module/packetenrichment/PacketEnrichmentImpl.java
+++ common/src/main/java/com/lunarclient/apollo/module/packetenrichment/PacketEnrichmentImpl.java
@@ -31,6 +31,7 @@ import com.lunarclient.apollo.event.packetenrichment.melee.ApolloPlayerAttackEve
 import com.lunarclient.apollo.event.packetenrichment.world.ApolloPlayerUseItemBucketEvent;
 import com.lunarclient.apollo.event.packetenrichment.world.ApolloPlayerUseItemEvent;
 import com.lunarclient.apollo.network.NetworkTypes;
+import com.lunarclient.apollo.option.Options;
 import com.lunarclient.apollo.packetenrichment.v1.PlayerAttackMessage;
 import com.lunarclient.apollo.packetenrichment.v1.PlayerChatCloseMessage;
 import com.lunarclient.apollo.packetenrichment.v1.PlayerChatOpenMessage;
@@ -55,77 +56,89 @@ public final class PacketEnrichmentImpl extends PacketEnrichmentModule {
     }
 
     private void onReceivePacket(ApolloReceivePacketEvent event) {
-        event.unpack(PlayerAttackMessage.class).ifPresent(packet -> {
-            ApolloPlayerAttackEvent playerAttackEvent = new ApolloPlayerAttackEvent(
-                event.getPlayer(),
-                NetworkTypes.fromProtobuf(packet.getPacketInfo().getInstantiationTime()),
-                NetworkTypes.fromProtobuf(packet.getTargetInfo()),
-                NetworkTypes.fromProtobuf(packet.getAttackerInfo()),
-                packet.getDistance()
-            );
+        Options options = this.getOptions();
 
-            EventBus.EventResult<ApolloPlayerAttackEvent> result = EventBus.getBus().post(playerAttackEvent);
+        if (options.get(PacketEnrichmentModule.PLAYER_CHAT_OPEN_EVENT)) {
+            event.unpack(PlayerAttackMessage.class).ifPresent(packet -> {
+                ApolloPlayerAttackEvent playerAttackEvent = new ApolloPlayerAttackEvent(
+                    event.getPlayer(),
+                    NetworkTypes.fromProtobuf(packet.getPacketInfo().getInstantiationTime()),
+                    NetworkTypes.fromProtobuf(packet.getTargetInfo()),
+                    NetworkTypes.fromProtobuf(packet.getAttackerInfo()),
+                    packet.getDistance()
+                );
 
-            for (Throwable throwable : result.getThrowing()) {
-                throwable.printStackTrace();
-            }
-        });
+                EventBus.EventResult<ApolloPlayerAttackEvent> result = EventBus.getBus().post(playerAttackEvent);
 
-        event.unpack(PlayerChatOpenMessage.class).ifPresent(packet -> {
-            ApolloPlayerChatOpenEvent playerChatOpenEvent = new ApolloPlayerChatOpenEvent(
-                event.getPlayer(),
-                NetworkTypes.fromProtobuf(packet.getPacketInfo().getInstantiationTime()),
-                NetworkTypes.fromProtobuf(packet.getPlayerInfo()));
+                for (Throwable throwable : result.getThrowing()) {
+                    throwable.printStackTrace();
+                }
+            });
+        }
 
-            EventBus.EventResult<ApolloPlayerChatOpenEvent> result = EventBus.getBus().post(playerChatOpenEvent);
+        if (options.get(PacketEnrichmentModule.PLAYER_CHAT_OPEN_EVENT)) {
+            event.unpack(PlayerChatOpenMessage.class).ifPresent(packet -> {
+                ApolloPlayerChatOpenEvent playerChatOpenEvent = new ApolloPlayerChatOpenEvent(
+                    event.getPlayer(),
+                    NetworkTypes.fromProtobuf(packet.getPacketInfo().getInstantiationTime()),
+                    NetworkTypes.fromProtobuf(packet.getPlayerInfo()));
 
-            for (Throwable throwable : result.getThrowing()) {
-                throwable.printStackTrace();
-            }
-        });
+                EventBus.EventResult<ApolloPlayerChatOpenEvent> result = EventBus.getBus().post(playerChatOpenEvent);
 
-        event.unpack(PlayerChatCloseMessage.class).ifPresent(packet -> {
-            ApolloPlayerChatCloseEvent playerChatCloseEvent = new ApolloPlayerChatCloseEvent(
-                event.getPlayer(),
-                NetworkTypes.fromProtobuf(packet.getPacketInfo().getInstantiationTime()),
-                NetworkTypes.fromProtobuf(packet.getPlayerInfo()));
+                for (Throwable throwable : result.getThrowing()) {
+                    throwable.printStackTrace();
+                }
+            });
+        }
 
-            EventBus.EventResult<ApolloPlayerChatCloseEvent> result = EventBus.getBus().post(playerChatCloseEvent);
+        if (options.get(PacketEnrichmentModule.PLAYER_CHAT_CLOSE_EVENT)) {
+            event.unpack(PlayerChatCloseMessage.class).ifPresent(packet -> {
+                ApolloPlayerChatCloseEvent playerChatCloseEvent = new ApolloPlayerChatCloseEvent(
+                    event.getPlayer(),
+                    NetworkTypes.fromProtobuf(packet.getPacketInfo().getInstantiationTime()),
+                    NetworkTypes.fromProtobuf(packet.getPlayerInfo()));
 
-            for (Throwable throwable : result.getThrowing()) {
-                throwable.printStackTrace();
-            }
-        });
+                EventBus.EventResult<ApolloPlayerChatCloseEvent> result = EventBus.getBus().post(playerChatCloseEvent);
 
-        event.unpack(PlayerUseItemMessage.class).ifPresent(packet -> {
-            ApolloPlayerUseItemEvent playerUseItemEvent = new ApolloPlayerUseItemEvent(
-                event.getPlayer(),
-                NetworkTypes.fromProtobuf(packet.getPacketInfo().getInstantiationTime()),
-                NetworkTypes.fromProtobuf(packet.getPlayerInfo()),
-                packet.getMainHand()
-            );
+                for (Throwable throwable : result.getThrowing()) {
+                    throwable.printStackTrace();
+                }
+            });
+        }
 
-            EventBus.EventResult<ApolloPlayerUseItemEvent> result = EventBus.getBus().post(playerUseItemEvent);
+        if (options.get(PacketEnrichmentModule.PLAYER_USE_ITEM_EVENT)) {
+            event.unpack(PlayerUseItemMessage.class).ifPresent(packet -> {
+                ApolloPlayerUseItemEvent playerUseItemEvent = new ApolloPlayerUseItemEvent(
+                    event.getPlayer(),
+                    NetworkTypes.fromProtobuf(packet.getPacketInfo().getInstantiationTime()),
+                    NetworkTypes.fromProtobuf(packet.getPlayerInfo()),
+                    packet.getMainHand()
+                );
 
-            for (Throwable throwable : result.getThrowing()) {
-                throwable.printStackTrace();
-            }
-        });
+                EventBus.EventResult<ApolloPlayerUseItemEvent> result = EventBus.getBus().post(playerUseItemEvent);
 
-        event.unpack(PlayerUseItemBucketMessage.class).ifPresent(packet -> {
-            ApolloPlayerUseItemBucketEvent playerUseItemBucketEvent = new ApolloPlayerUseItemBucketEvent(
-                event.getPlayer(),
-                NetworkTypes.fromProtobuf(packet.getPacketInfo().getInstantiationTime()),
-                NetworkTypes.fromProtobuf(packet.getPlayerInfo()),
-                NetworkTypes.fromProtobuf(packet.getRayTraceResult())
-            );
+                for (Throwable throwable : result.getThrowing()) {
+                    throwable.printStackTrace();
+                }
+            });
+        }
 
-            EventBus.EventResult<ApolloPlayerUseItemBucketEvent> result = EventBus.getBus().post(playerUseItemBucketEvent);
+        if (options.get(PacketEnrichmentModule.PLAYER_USE_ITEM_BUCKET_EVENT)) {
+            event.unpack(PlayerUseItemBucketMessage.class).ifPresent(packet -> {
+                ApolloPlayerUseItemBucketEvent playerUseItemBucketEvent = new ApolloPlayerUseItemBucketEvent(
+                    event.getPlayer(),
+                    NetworkTypes.fromProtobuf(packet.getPacketInfo().getInstantiationTime()),
+                    NetworkTypes.fromProtobuf(packet.getPlayerInfo()),
+                    NetworkTypes.fromProtobuf(packet.getRayTraceResult())
+                );
 
-            for (Throwable throwable : result.getThrowing()) {
-                throwable.printStackTrace();
-            }
-        });
+                EventBus.EventResult<ApolloPlayerUseItemBucketEvent> result = EventBus.getBus().post(playerUseItemBucketEvent);
+
+                for (Throwable throwable : result.getThrowing()) {
+                    throwable.printStackTrace();
+                }
+            });
+        }
     }
 
 }
