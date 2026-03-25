ec61fa28b317ce1343849a37a07d4c75ad3324cd
ItsNature <matej.bucaric@gmail.com>
2026-03-25 02:35:55 +0100

    server links api (WIP)

--- api/src/main/java/com/lunarclient/apollo/module/serverlink/ServerLinkModule.java
+++ api/src/main/java/com/lunarclient/apollo/module/serverlink/ServerLinkModule.java
@@ -23,8 +23,11 @@
  */
 package com.lunarclient.apollo.module.serverlink;
 
+import com.lunarclient.apollo.common.icon.ResourceLocationIcon;
 import com.lunarclient.apollo.module.ApolloModule;
 import com.lunarclient.apollo.module.ModuleDefinition;
+import com.lunarclient.apollo.recipients.Recipients;
+import java.util.List;
 import org.jetbrains.annotations.ApiStatus;
 
 /**
@@ -38,5 +41,90 @@ import org.jetbrains.annotations.ApiStatus;
 @ApiStatus.NonExtendable
 @ModuleDefinition(id = "server_link", name = "Server Link")
 public abstract class ServerLinkModule extends ApolloModule {
+
+    /**
+     * Overrides the server link menu title image for the {@link Recipients}.
+     *
+     * <p>The provided {@link ResourceLocationIcon} will be displayed in place of the default
+     * menu title.</p>
+     *
+     * <p>The resource location must reference a valid client-side asset using the standard
+     * namespace format:</p>
+     *
+     * <pre>
+     * minecraft:textures/item/apple.png
+     * lunar:logo/logo-100x100.png
+     * </pre>
+     *
+     * <p>For optimal results, a square image (e.g. 128x128) is recommended.</p>
+     *
+     * @param recipients the recipients that are receiving the packet
+     * @param icon       the resource location icon
+     * @since 1.2.4
+     */
+    public abstract void overrideServerLinkResource(Recipients recipients, ResourceLocationIcon icon);
+
+    /**
+     * Resets the server link menu title image for the {@link Recipients}.
+     *
+     * <p>Reverts back to displaying the default menu title.</p>
+     *
+     * @param recipients the recipients that are receiving the packet
+     * @since 1.2.4
+     */
+    public abstract void resetServerLinkResource(Recipients recipients);
+
+    /**
+     * Adds or updates the {@link ServerLink} for the {@link Recipients}.
+     *
+     * @param recipients the recipients that are receiving the packet
+     * @param serverLink the server link
+     * @since 1.2.4
+     */
+    public abstract void addServerLink(Recipients recipients, ServerLink serverLink);
+
+    /**
+     * Adds or updates the {@link ServerLink}s for the {@link Recipients}.
+     *
+     * @param recipients  the recipients that are receiving the packet
+     * @param serverLinks the server links
+     * @since 1.2.4
+     */
+    public abstract void addServerLink(Recipients recipients, List<ServerLink> serverLinks);
+
+    /**
+     * Removes the {@link ServerLink} from the {@link Recipients}.
+     *
+     * @param recipients   the recipients that are receiving the packet
+     * @param serverLinkId the server link id
+     * @since 1.2.4
+     */
+    public abstract void removeServerLink(Recipients recipients, String serverLinkId);
+
+    /**
+     * Removes the {@link ServerLink} from the {@link Recipients}.
+     *
+     * @param recipients the recipients that are receiving the packet
+     * @param serverLink the server link
+     * @since 1.2.4
+     */
+    public abstract void removeServerLink(Recipients recipients, ServerLink serverLink);
+
+    /**
+     * Removes the {@link ServerLink}s from the {@link Recipients}.
+     *
+     * @param recipients    the recipients that are receiving the packet
+     * @param serverLinkIds the server link ids
+     * @since 1.2.4
+     */
+    public abstract void removeServerLink(Recipients recipients, List<String> serverLinkIds);
+
+    /**
+     * Resets all {@link ServerLink}s for the {@link Recipients}.
+     *
+     * @param recipients the recipients that are receiving the packet
+     * @since 1.2.4
+     */
+    public abstract void resetServerLinks(Recipients recipients);
 
 }
