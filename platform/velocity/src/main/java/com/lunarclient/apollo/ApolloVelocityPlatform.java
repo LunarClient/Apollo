321cee5704d2cb9b57b710567d5addc1aadabdce
Matej Bučarić <matej.bucaric@gmail.com>
2026-03-26 17:54:18 +0100

    Bump to 1.2.4 (#263)

--- platform/velocity/src/main/java/com/lunarclient/apollo/ApolloVelocityPlatform.java
+++ platform/velocity/src/main/java/com/lunarclient/apollo/ApolloVelocityPlatform.java
@@ -105,7 +105,7 @@ import lombok.Getter;
 @Plugin(
     id = "apollo",
     name = "Apollo-Velocity",
-    version = "1.2.3",
+    version = "1.2.4",
     url = "https://moonsworth.com",
     description = "Implementation of Apollo for Velocity",
     authors = {"Moonsworth"}
