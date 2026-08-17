9a11430ce0c99725a7251d99b2e851c08e827f37
Matej Bučarić <matej.bucaric@gmail.com>
2026-08-17 15:01:04 +0200

    Bump to 1.2.9 (#308)

--- platform/minestom/src/main/java/com/lunarclient/apollo/ApolloMinestomPlatform.java
+++ platform/minestom/src/main/java/com/lunarclient/apollo/ApolloMinestomPlatform.java
@@ -253,7 +253,7 @@ public final class ApolloMinestomPlatform implements ApolloPlatform {
 
     @Override
     public String getApolloVersion() {
-        return "1.2.8";
+        return "1.2.9";
     }
 
     @Override
